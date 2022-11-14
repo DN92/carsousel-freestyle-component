13825d9734cf0ed9ff8da7fe3099eec6265dd810
DN92 <anatoly.tsinker12@gmail.com>
2022-11-14 17:42:00 -0500

    added demo components; and images for testing carousel

---
+++ src/components/ButtonDemo.tsx
@@ -0,0 +1,37 @@
+import { MantineProvider, Button, Badge, ButtonStylesParams } from '@mantine/core';
+
+function Demo() {
+  return (
+    <MantineProvider
+      theme={{
+        components: {
+          Button: {
+            // Subscribe to theme and component params
+            styles: (theme, params: ButtonStylesParams) => ({
+              root: {
+                height: 42,
+                padding: '0 30px',
+                backgroundColor:
+                  params.variant === 'filled'
+                    ? theme.colors[params.color || theme.primaryColor][9]
+                    : undefined,
+              },
+            }),
+          },
+
+          Badge: {
+            // Use raw styles object if you do not need theme dependency
+            styles: {
+              root: { borderWidth: 2 },
+            },
+          },
+        },
+      }}
+    >
+      <Button variant="outline">Outline button</Button>
+      <Button variant="filled" color="cyan">Filled button</Button>
+      <Badge variant="dot">Dot badge</Badge>
+    </MantineProvider>
+  );
+}
+export default Demo
